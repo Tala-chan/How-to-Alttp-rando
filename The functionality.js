ab44e617827380eae0ead0bbc4ee87f202584c06
Avital Vasserman <vasserman.avital@elev.ga.ntig.se>
2022-03-09 22:27:35 +0100

    first commit

---
+++ The functionality.js
@@ -0,0 +1,7 @@
+const btnListen = document.querySelector(".lil");
+
+btnListen.addEventListener(click, "nextImg");
+
+function nextImg() {
+  document.getElementById("image").style.display = "block";
+}
