f619b2a78ddae115b3c2c901134f49ae2228b933
Avital Vasserman <vasserman.avital@elev.ga.ntig.se>
2022-04-26 21:46:27 +0200

    Shame

--- The functionality.js
+++ The functionality.js
@@ -1,7 +1,77 @@
-const btnListen = document.querySelector(".lil");
-
-btnListen.addEventListener(click, "nextImg");
-
+let x;
+x = 0;
 function nextImg() {
-  document.getElementById("image").style.display = "block";
+  x++;
+  switch (x) {
+    case 0:
+      document.getElementById("00").style.display = "block";
+      document.getElementById("01").style.display = "none";
+      document.getElementById("02").style.display = "none";
+      document.getElementById("03").style.display = "none";
+      document.getElementById("prevArrow").style.display = "none";
+      document.getElementById("nextArrow").style.display = "block";
+      break;
+    case 1:
+      document.getElementById("01").style.display = "block";
+      document.getElementById("00").style.display = "none";
+      document.getElementById("02").style.display = "none";
+      document.getElementById("03").style.display = "none";
+      document.getElementById("prevArrow").style.display = "block";
+      document.getElementById("nextArrow").style.display = "block";
+
+      break;
+    case 2:
+      document.getElementById("02").style.display = "block";
+      document.getElementById("00").style.display = "none";
+      document.getElementById("01").style.display = "none";
+      document.getElementById("03").style.display = "none";
+      document.getElementById("prevArrow").style.display = "block";
+      document.getElementById("nextArrow").style.display = "block";
+      break;
+    case 3:
+      document.getElementById("03").style.display = "block";
+      document.getElementById("00").style.display = "none";
+      document.getElementById("01").style.display = "none";
+      document.getElementById("02").style.display = "none";
+      document.getElementById("prevArrow").style.display = "block";
+      document.getElementById("nextArrow").style.display = "none";
+      break;
+  }
+}
+function prevImg() {
+  x--;
+  switch (x) {
+    case 0:
+      document.getElementById("00").style.display = "block";
+      document.getElementById("01").style.display = "none";
+      document.getElementById("02").style.display = "none";
+      document.getElementById("03").style.display = "none";
+      document.getElementById("where").style.margin = 50;
+      document.getElementById("prevArrow").style.display = "none";
+      document.getElementById("nextArrow").style.display = "block";
+      break;
+    case 1:
+      document.getElementById("01").style.display = "block";
+      document.getElementById("00").style.display = "none";
+      document.getElementById("02").style.display = "none";
+      document.getElementById("03").style.display = "none";
+      document.getElementById("prevArrow").style.display = "block";
+      document.getElementById("nextArrow").style.display = "block";
+      break;
+    case 2:
+      document.getElementById("02").style.display = "block";
+      document.getElementById("00").style.display = "none";
+      document.getElementById("01").style.display = "none";
+      document.getElementById("03").style.display = "none";
+      document.getElementById("prevArrow").style.display = "block";
+      document.getElementById("nextArrow").style.display = "block";
+    case 3:
+      document.getElementById("03").style.display = "block";
+      document.getElementById("00").style.display = "none";
+      document.getElementById("01").style.display = "none";
+      document.getElementById("02").style.display = "none";
+      document.getElementById("prevArrow").style.display = "block";
+      document.getElementById("nextArrow").style.display = "none";
+      break;
+  }
 }
